95a73de43d7ce40aab85836bdd5d995c1df716a3
Samin Riasat <nayel71@gmail.com>
2023-02-13 20:53:56 -0500

    removed redundant statement

--- quadratic_integer.py
+++ quadratic_integer.py
@@ -5,8 +5,7 @@ def is_square(num):
     """Return True iff num is a perfect square."""
     if num <= 0:
         return num == 0
-    num_len = len(str(num))
-    lo = 10**((num_len-1)//2)
+    lo = 10**((len(str(num))-1)//2)
     hi = lo * 10
     while lo <= hi:
         mid = lo + (hi-lo)//2
